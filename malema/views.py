c0f722b76cffca93d59ab3b64e919876ed8ec2cf
lukho <plamlukho1@gmail.com>
2024-05-08 13:21:22 +0200

    added requirements.txt and gitignore

--- malema/views.py
+++ malema/views.py
@@ -6,15 +6,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def biography(request):
-    """displays biography page"""
     return render(request, 'biography.html')
 
 def life_and_education(request):
-    """displays life and education page"""
     return render(request, 'life_and_education.html')
 
 def anc_youth(request):
-    """displays anc youth page"""
     return render(request, 'anc_youth.html')
 
 
