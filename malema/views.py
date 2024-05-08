5ec41db94d18de8d062a25f9bf72433e2b17345b
lukho <plamlukho1@gmail.com>
2024-05-08 13:32:31 +0200

    added docstrings

--- malema/views.py
+++ malema/views.py
@@ -6,12 +6,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def biography(request):
+    """this function displays the biography of malema """
     return render(request, 'biography.html')
 
 def life_and_education(request):
+    """this function displays the life and education of malema """
     return render(request, 'life_and_education.html')
 
 def anc_youth(request):
+    """this function displays the anc youth of malema that he was part of ."""
     return render(request, 'anc_youth.html')
 
 
